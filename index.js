0cd8089711f0073688350e8e78ca6ea5ee8f4c70
Texas <79846517+Lingz-ui@users.noreply.github.com>
2022-05-25 20:11:01 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const Canvas = require("canvas");
+Canvas.registerFont(`${__dirname}/canvas/rpg.ttf`, { family: "myFont" });
+Canvas.registerFont(`${__dirname}/canvas/novamono.ttf`, { family: "NVM" });
+
+
+
+module.exports.Welcome = require('./canvas/Welcome');
+module.exports.Goodbye = require('./canvas/Goodbye');
+module.exports.Profile = require('./canvas/Profile');
+
+
