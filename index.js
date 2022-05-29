5bc67b41555f33321517f90ae6a3aac5eef44b9e
Texas <79846517+Lingz-ui@users.noreply.github.com>
2022-05-29 17:52:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ Canvas.registerFont(`${__dirname}/canvas/rpg.ttf`, { family: "myFont" });
 Canvas.registerFont(`${__dirname}/canvas/novamono.ttf`, { family: "NVM" });
 
 
-
+module.exports.About = require('./canvas/About');
 module.exports.Welcome = require('./canvas/Welcome');
 module.exports.Goodbye = require('./canvas/Goodbye');
 module.exports.Profile = require('./canvas/Profile');
